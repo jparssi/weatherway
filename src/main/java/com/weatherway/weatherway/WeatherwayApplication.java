e7b3244dc72ca54d0688cd2ef3d4f70bbdd5e966
Juho <parssinenjuho@gmail.com>
2024-10-13 17:41:19 +0300

    First commit

---
+++ src/main/java/com/weatherway/weatherway/WeatherwayApplication.java
@@ -0,0 +1,13 @@
+package com.weatherway.weatherway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weatherway/weatherway/WeatherwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weatherway.weatherway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
